4b51edfcaa641d46ada2667f021effc943f9314a
Jaiyeola Opeoluwa <73923097+jAyJOet@users.noreply.github.com>
2023-09-10 17:57:04 -0800

    Update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from time import strftime
+from time import strftime,strptime
 from flask import Flask,request,jsonify
 import datetime
 
@@ -8,16 +8,40 @@ app = Flask(__name__)
 def get_hng_data():
     slack_name = request.args.get('slack_name',
                                   default='jAyJOet',
-                                  type='str',)
+                                 )
     track = request.args.get('track',
-                             default='Backend',
-                             type='str',)
+                             default='backend',                   
+                            )
+
+    present_day =  datetime.datetime.utcnow().strftime('%A')
+
     
 
-    present_day = datetime.datetime.utcnow().strftime('%A   %D')
+    github_repo_url = 'https://github.com/jAyJOet/HNGx_Projects.git'
+    github_file_url = 'https://github.com/jAyJOet/HNGx_Projects/blob/master/main.py'
 
-    date = datetime.datetime.utcnow()
-    utc_time = strftime('%H:%M:%S UTC')
+
+    hngx_user={
+        "slack_name": slack_name, 
+        "current_day": present_day,
+        "utc_time": f'{datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")}',
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200,
+                                }
+    if not slack_name or not track:
+        return jsonify({'error': 'Two arguments are required'}), 400
+
+    if slack_name == hngx_user['slack_name'] and track == hngx_user['track']:
+        return jsonify(hngx_user), 200
+    else:
+        return jsonify({'error': 'One or both parameters not found'}), 404
+       
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5010, debug=True)
 
     github_repo_url = 'https://github.com/jAyJOet/HNGx_Projects.git'
     github_file_url = 'https://github.com/jAyJOet/HNGx_Projects/blob/jAyJOet/main.py'
